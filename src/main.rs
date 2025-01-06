2238136d83dbfafb444ad4bc3c8a86775a3caed5
damccull <401104+damccull@users.noreply.github.com>
2025-01-05 17:06:13 -1000

    Add dog app content

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use dioxus::prelude::*;
 
+static CSS: Asset = asset!("/assets/main.css");
 
 fn main() {
     dioxus::launch(App);
@@ -8,7 +9,19 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
+        document::Stylesheet {href: CSS}
         div {
+            id: "title",
+            h1 { "HotDog! 🌭"}
+        }
+        div {
+            id: "dogview",
+            img { src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg" }
+        }
+        div {
+            id: "buttons",
+            button { id:"skip", "skip"}
+            button { id:"save", "save"}
         }
     }
 }
